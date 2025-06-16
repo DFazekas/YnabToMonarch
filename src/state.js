899524f650e40424d8377223cc055bc3c3ae2978
Devon Fazekas <fazekas.devon@gmail.com>
2025-06-16 03:47:45 -0400

    Remove placeholders from state

--- src/state.js
+++ src/state.js
@@ -1,8 +1,8 @@
 export default {
-  deviceUuid: '550e8400-e29b-51d4-a716-446655440000', //null,
+  deviceUuid: null,
   awaitingOtp: false,
-  credentials: { email: 'fazekas.devon+monarch4@gmail.com', password: 'O%s4NiR8Q1dUHq', otp: '' },
-  apiToken: "d9385c8e239d7cc3bd9cdb998c02187acf2b683d5da062e2d1edb64bfe716dac",
+  credentials: { email: '', password: '', otp: '' },
+  apiToken: "",
   monarchAccounts: null,
   accounts: {}
 };
